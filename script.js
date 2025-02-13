cb1d574d0487d427579bd3af9b8e534fd5fd1abd
Mohd Adil <90774182+adil088@users.noreply.github.com>
2025-02-14 02:08:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	 let res = "";
+  
+      for(let i = 0; i < s.length; i++){
+          if(s.charAt(i) === " " && res !== ""){
+              break;
+          }else{
+              res = res + s.charAt(i);
+          }
+      }
+  
+      return res;
 }
 
 // Do not change the code below
